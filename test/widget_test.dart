6eb2b21f3ed38f4fc13083f4cd994a92a7616b3c
unknown <mohammed.n.shawish@gmail.com>
2023-07-23 02:04:38 +0300

    virsion 2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:travel/main.dart';
 
 void main() {
-  testWidgets('Counterrrr increments smoke test', (WidgetTester tester) async {
+  testWidgets('virson 1 increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
